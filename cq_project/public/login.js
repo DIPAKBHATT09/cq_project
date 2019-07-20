bde44812710b636ff566534cfd2acd820b05a956
himanshugarg01 <himanshu171321.cse@chitkara.edu.in>
2019-07-20 11:27:59 +0530

    project added

---
+++ cq_project/public/login.js
@@ -0,0 +1,35 @@
+var username=document.getElementById('username');
+var password=document.getElementById("password");
+var log=document.getElementById("logbtn");
+
+
+
+log.addEventListener("click",function()
+{
+  if(username.value!=""&&password.value!="")
+  {
+
+
+    var request = new XMLHttpRequest();
+      request.addEventListener('load', function()
+      {
+        var data=JSON.parse(request.responseText);
+        console.log(data);
+        if(data.length>0)
+        {
+          alert("sucessful");
+        }
+        else {
+          alert("wrong username or password");
+          password.value="";
+        }
+      });
+      request.open('POST', '/log');
+      request.setRequestHeader("Content-Type", "application/json");
+      request.send(JSON.stringify({'username' : username.value , 'password' : password.value}));
+  }
+  else {
+    alert("all fields are required");
+  }
+
+});
---
+++ cq_project/public/menu.js
@@ -0,0 +1,21 @@
+/*var adduser=document.getElementById("adduser");
+var userlist=document.getElementById("userlist");
+var communitylist=document.getElementById("communitylist");
+var switchasuser=document.getElementById("switchasuser");
+var tagpanel=document.getElementById("tagpanel");
+var changepassword=document.getElementById("changepassword");
+var logout=document.getElementById("logout");
+
+
+adduser.addEventListener("click",function()
+{
+  var request = new XMLHttpRequest();
+    request.addEventListener('load', function()
+    {
+
+    });
+    request.open('GET', '/getadduser');
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send();
+});
+*/
